dc38b2d75256556489890597a8c1350bd45c74bb
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-06-26 21:54:02 +0200

    feat: send message on enter

--- src/components/chat/form.tsx
+++ src/components/chat/form.tsx
@@ -6,11 +6,21 @@ import ReactTextareaAutosize from "react-textarea-autosize"
 import SendIcon from "../assets/send-icon"
 import SpinnerIcon from "../assets/spinner-icon"
 
+import type { KeyboardEvent } from "react"
+
 const Form = ({ id, theme }: { id: string; theme: string }) => {
   const { isLoading, input, handleInputChange, handleSubmit } = useChat({
     id,
     body: { theme },
   })
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault()
+      const formEvent = new Event("submit", { bubbles: true, cancelable: true })
+      e.currentTarget.form?.dispatchEvent(formEvent)
+    }
+  }
 
   return (
     <div className="form-container">
@@ -24,6 +34,7 @@ const Form = ({ id, theme }: { id: string; theme: string }) => {
           cacheMeasurements
           onChange={handleInputChange}
           placeholder="Envoyez un message..."
+          onKeyDown={handleKeyDown}
         />
         <button type="submit">
           {isLoading ? <SpinnerIcon /> : <SendIcon />}
