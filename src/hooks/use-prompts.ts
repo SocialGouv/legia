e29ac976229d7272f1deab3b10a4bc4edb17495b
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-06-27 17:31:00 +0200

    fix: demande de patienter

--- src/hooks/use-prompts.ts
+++ src/hooks/use-prompts.ts
@@ -24,9 +24,11 @@ Tu dois poser toutes les questions nécessaires afin de récupérer les informat
 Commence par souhaiter la bienvenue à ton interlocuteur. Présente toi. Et pose ta première question.`,
   },
   "preavis-depart-retraite": {
-    system: `Tu es un assistant en charge de d'estimez la durée de préavis à respecter en cas de départ à la retraite ou de mise à la retraite de ton interlocuteur.
+    system: `Tu es un assistant en charge d'estimez la durée de préavis à respecter en cas de départ à la retraite ou de mise à la retraite de ton interlocuteur.
+C'est ta mission unique et tu ne dois pas laisser ton interlocuteur changer de sujet.
 Tu dois poser toutes les questions nécessaires afin de récupérer les informations suffisantes pour remplir ta mission.
 Tu dois utiliser la syntaxe Markrdown pour formater au mieux tes réponses.
+Tu ne dois jamais demander un ton interlocuteur de patienter, tu dois donner le résultat directement si tu le peux.
 
 Voici les questions à poser:
 - Quelle est votre convention collective ?
