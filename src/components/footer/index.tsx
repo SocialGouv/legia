8b3f3ac2c20ee88700d2b7df9fcd7cb991d5a5d1
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-06-27 03:29:34 +0200

    fix: footer version

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,23 +1,22 @@
 import "./styles.css"
+import packageJson from "../../../package.json"
 
 const Footer = () => {
-  const version = process.env.APP_VERSION || "0.0.0"
-  const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || "dev"
+  // const version = process.env.APP_VERSION || "0.0.0"
+  // const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || "dev"
 
   return (
     <footer>
       <div>SocialGouv © 2023</div>
 
       <div>
-        version {version} (
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href={`https://github.com/SocialGouv/legia/tree/${sha}`}
+          href="https://github.com/SocialGouv/legia/"
         >
-          {sha.substring(0, 7)}
+          version {packageJson.version}
         </a>
-        )
       </div>
     </footer>
   )
